ca476347189600e7042ffce5d72e243911b88033
dillonl <dlee@genetics.utah.edu>
2015-01-16 16:57:20 -0500

    Fixed CMake files to fully remove vcflib and tabix. Removed vcflib and tabix files

--- src/tools/gwiz.cpp
+++ src/tools/gwiz.cpp
@@ -1,11 +1,11 @@
 #include "utils/Parameters.h"
-#include "fastahack/Fasta.h"
-#include "Variant.h"
+// #include "fastahack/Fasta.h"
+// #include "Variant.h"
 
 int main(int argc, char** argv)
 {
-	vcf::VariantCallFile vcfFile;
-	vcf::Variant var(vcfFile);
-	FastaReference test;
+	// vcf::VariantCallFile vcfFile;
+	// vcf::Variant var(vcfFile);
+	// FastaReference test;
 	return 0;
 }
