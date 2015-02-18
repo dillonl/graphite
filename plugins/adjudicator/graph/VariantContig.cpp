8cfdd35c6d81e51f44e1e11f9dbf8bf4688708c6
Dillon Lee <dlee@Dillons-iMac.local>
2015-02-17 17:48:30 -0700

    still working on variantcontig

--- plugins/adjudicator/graph/VariantContig.cpp
+++ plugins/adjudicator/graph/VariantContig.cpp
@@ -1,16 +1,28 @@
 #include "VariantContig.h"
 
+#include <map>
+
 namespace gwiz
 {
 namespace adjudicator
 {
 
-	VariantContig::VariantContig()
+	VariantContig::VariantContig(Region::SharedPtr region, IReference::SharedPtr referencePtr, vg::ReferenceNode::SharedPtr startReferenceNode, vg::ReferenceNode::SharedPtr endReferenceNode) :
+		m_region(region),
+		m_reference_ptr(referencePtr),
+		m_start_reference_node(startReferenceNode),
+		m_end_reference_node(endReferenceNode)
 	{
 	}
 
 	VariantContig::~VariantContig()
 	{
+	}
+
+	void VariantContig::buildVariantContig()
+	{
+		this->m_contigs.clear();
+
 	}
 
 } // namespace adjudicator
--- plugins/adjudicator/graph/VariantContig.h
+++ plugins/adjudicator/graph/VariantContig.h
@@ -3,6 +3,10 @@
 
 #include <boost/noncopyable.hpp>
 
+
+#include "core/reference/IReference.h"
+#include "vg/graph/ReferenceNode.h"
+
 namespace gwiz
 {
 namespace adjudicator
@@ -11,8 +15,17 @@ namespace adjudicator
 	class VariantContig : boost::noncopyable
 	{
 	public:
-		VariantContig();
+        VariantContig(Region::SharedPtr region, IReference::SharedPtr referencePtr, vg::ReferenceNode::SharedPtr startReferenceNode, vg::ReferenceNode::SharedPtr endReferenceNode);
 		~VariantContig();
+
+		void buildVariantContig();
+
+	private:
+		Region::SharedPtr m_region;
+		IReference::SharedPtr m_reference_ptr;
+        vg::ReferenceNode::SharedPtr m_start_reference_node;
+        vg::ReferenceNode::SharedPtr m_end_reference_node;
+		std::vector< std::string > m_contigs;
 	};
 
 } // namespace adjudicator
